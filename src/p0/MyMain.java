6f7b27b46dda8591f8ea61c23a5326a75c9c0c8a
Leonidas Varouchas <varleonidas@gmail.com>
2018-04-14 14:46:41 +0300

    Clearing

--- src/p0/MyMain.java
+++ src/p0/MyMain.java
@@ -4,52 +4,7 @@ public class MyMain {
 
 	public static void main(String[] args) {
 		Menu m =new Menu();
-//		Users users= Users.geUsersInst();
-//		
-//		User leon=users.getUsers().get(2);
-//		leon=(User)leon;
-//		System.out.println(leon instanceof User);
 		m.startMenu();
-		
-		
-		//m.mainMenu();
-		//m.processLoop();
-		
-//		List<User> users = new ArrayList<User>();
-//
-//		Admin admin = new Admin("SuperAd", "SuperAd@email.com", "123123");
-//
-//		users.add(new User("Leonidas", "mail@domain.com", "123123"));
-//		users.add(new User("Maria", "maria@domain.com", "123123"));
-//		users.add(new Editor("jim", "jim@email.com", "123123"));
-//		users.add(admin);
-//
-//		users.set(0, admin.updateUserRole(users.get(0), Role.EDITOR));
-//
-//		Iterator<User> usersIter = users.iterator();
-//
-//		while (usersIter.hasNext()) {
-//			System.out.println(usersIter.next());
-//		}
-
-		// Scanner sc= new Scanner(System.in);
-		// Login u= new Login();
-
-		// Menus.welcomeMenu();
-		// int sel=sc.nextInt();
-		// sc.close();
-		// switch (sel) {
-		// case 1:
-		// //Login
-		// if (Login.login(name,password)) {}
-		// break;
-		//
-		// case 2:
-		// //Register
-		// if(Login.register(name, password, email, role)) {}
-		// break;
-		// }
-
 	}
 
 }
